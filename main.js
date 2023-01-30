9557f23c4390bd4a7da00394225478ab42807ad6
Hedy Yanet Daza Bocarejo <123220282+HedyYanetDazaBocarejo38@users.noreply.github.com>
2023-01-30 09:13:50 -0500

    main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+// let Apellido = " Daza Bocarejo";
+// let Altura = 1.62
+// console.log(Me llamo ); $%{ nombre};
+// console.log(Mi altura es ); $%{ altura};
+ let  apellido =  "Daza";
+let  ciudad= "floridablanca";
+let edad= "38";
+
+console.log('El apellido es $ {Daza}');
+console.log('La ciudad es  $ {Floridablanca}');
+console.log('la Edad es $ {38}'); 
